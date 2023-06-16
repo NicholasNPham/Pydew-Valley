d7ac9aab90c56e857c02c38a9dde107e1baf2d4d
Nicholas Pham <132528074+NicholasNPham@users.noreply.github.com>
2023-06-16 17:49:56 -0400

    Add files via upload

--- code/player.py
+++ code/player.py
@@ -4,145 +4,194 @@ from support import *
 from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, group):
-        super().__init__(group)
+	def __init__(self, pos, group, collision_sprites, tree_sprites):
+		super().__init__(group)
 
-        self.import_assets()
-        self.status = 'down_idle'
-        self.frame_index = 0
+		self.import_assets()
+		self.status = 'down_idle'
+		self.frame_index = 0
 
-        # General Setup
-        self.image = self.animations[self.status][self.frame_index]
-        self.rect = self.image.get_rect(center=pos)
-        self.z = LAYERS['main']
+		# general setup
+		self.image = self.animations[self.status][self.frame_index]
+		self.rect = self.image.get_rect(center = pos)
+		self.z = LAYERS['main']
 
-        # Move Attributes
-        self.direction = pygame.math.Vector2()
-        self.pos = pygame.math.Vector2(self.rect.center)
-        self.speed = 200
+		# movement attributes
+		self.direction = pygame.math.Vector2()
+		self.pos = pygame.math.Vector2(self.rect.center)
+		self.speed = 200
 
-        # timers
-        self.timers = {
-            'tool use': Timer(350, self.use_tool),
-            'tool switch': Timer(200),
-            'seed use': Timer(350, self.use_seed),
-            'seed switch': Timer(200)
-        }
+		# collision
+		self.hitbox = self.rect.copy().inflate((-126,-70))
+		self.collision_sprites = collision_sprites
 
-        # Tools
-        self.tools = ['hoe', 'axe', 'water']
-        self.tool_index = 0
-        self.selected_tool = self.tools[self.tool_index]
+		# timers 
+		self.timers = {
+			'tool use': Timer(350,self.use_tool),
+			'tool switch': Timer(200),
+			'seed use': Timer(350,self.use_seed),
+			'seed switch': Timer(200),
+		}
 
-        # Seeds
-        self.seeds = ['corn', 'tomato']
-        self.seed_index = 0
-        self.selected_seed = self.seeds[self.seed_index]
+		# tools 
+		self.tools = ['hoe','axe','water']
+		self.tool_index = 0
+		self.selected_tool = self.tools[self.tool_index]
 
-    def use_tool(self):
-        pass
-        # print(self.selected_tool)
+		# seeds 
+		self.seeds = ['corn', 'tomato']
+		self.seed_index = 0
+		self.selected_seed = self.seeds[self.seed_index]
 
-    def use_seed(self):
-        pass
+		# interaction
+		self.tree_sprites = tree_sprites
 
-    def import_assets(self):
-        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
-                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
-                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
-                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
-                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
-        for animation in self.animations.keys():
-            full_path = '../graphics/character/' + animation
-            self.animations[animation] = import_folder(full_path)
+	def use_tool(self):
+		print('tool use')
+		if self.selected_tool == 'hoe':
+			pass
+		
+		if self.selected_tool == 'axe':
+			for tree in self.tree_sprites.sprites():
+				if tree.rect.collidepoint(self.target_pos):
+					tree.damage()
+		
+		if self.selected_tool == 'water':
+			pass
 
-    def animate(self,dt):
-        self.frame_index += 4 * dt
-        if self.frame_index >= len(self.animations[self.status]):
-            self.frame_index = 0
+	def get_target_pos(self):
 
-        self.image = self.animations[self.status][int(self.frame_index)]
+		self.target_pos = self.rect.center + PLAYER_TOOL_OFFSET[self.status.split('_')[0]]
 
-    def input(self):
-        keys = pygame.key.get_pressed()
 
-        if not self.timers['tool use'].active:
-        # Directions
-            if keys[pygame.K_UP]:
-                self.direction.y = -1
-                self.status = 'up'
-            elif keys[pygame.K_DOWN]:
-                self.direction.y = 1
-                self.status = 'down'
-            else:
-                self.direction.y = 0
+	def use_seed(self):
 
-            if keys[pygame.K_RIGHT]:
-                self.direction.x = 1
-                self.status = 'right'
-            elif keys[pygame.K_LEFT]:
-                self.direction.x = -1
-                self.status = 'left'
-            else:
-                self.direction.x = 0
+		pass
 
-            # Tool Uses
-            if keys[pygame.K_SPACE]:
-                self.timers['tool use'].activate()
-                self.direction = pygame.math.Vector2()
-                self.frame_index = 0
+	def import_assets(self):
+		self.animations = {'up': [],'down': [],'left': [],'right': [],
+						   'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+						   'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
+						   'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
+						   'right_water':[],'left_water':[],'up_water':[],'down_water':[]}
 
-            # Change Tools
-            if keys[pygame.K_q] and not self.timers['tool switch'].active:
-                self.timers['tool switch'].activate()
-                self.tool_index += 1
-                self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
-                self.selected_tool = self.tools[self.tool_index]
+		for animation in self.animations.keys():
+			full_path = '../graphics/character/' + animation
+			self.animations[animation] = import_folder(full_path)
 
-            # Seed Use
-            if keys[pygame.K_LCTRL]:
-                self.timers['seed use'].activate()
-                self.direction = pygame.math.Vector2()
-                self.frame_index = 0
+	def animate(self,dt):
+		self.frame_index += 4 * dt
+		if self.frame_index >= len(self.animations[self.status]):
+			self.frame_index = 0
 
-            # Change Seed
-            if keys[pygame.K_e] and not self.timers['seed switch'].active:
-                self.timers['seed switch'].activate()
-                self.seed_index += 1
-                self.seed_index = self.seed_index if self.seed_index < len(self.seeds) else 0
-                self.selected_seed = self.seeds[self.seed_index]
+		self.image = self.animations[self.status][int(self.frame_index)]
 
-    def get_status(self):
+	def input(self):
+		keys = pygame.key.get_pressed()
 
-        # Idle
-        if self.direction.magnitude() == 0:
-            self.status = self.status.split('_')[0] + '_idle'
+		if not self.timers['tool use'].active:
+			# directions 
+			if keys[pygame.K_UP]:
+				self.direction.y = -1
+				self.status = 'up'
+			elif keys[pygame.K_DOWN]:
+				self.direction.y = 1
+				self.status = 'down'
+			else:
+				self.direction.y = 0
 
-        # Tool Use
-        if self.timers['tool use'].active:
-            self.status = self.status.split('_')[0] + '_' + self.selected_tool
+			if keys[pygame.K_RIGHT]:
+				self.direction.x = 1
+				self.status = 'right'
+			elif keys[pygame.K_LEFT]:
+				self.direction.x = -1
+				self.status = 'left'
+			else:
+				self.direction.x = 0
 
-    def updater_timers(self):
-        for timer in self.timers.values():
-            timer.update()
+			# tool use
+			if keys[pygame.K_SPACE]:
+				self.timers['tool use'].activate()
+				self.direction = pygame.math.Vector2()
+				self.frame_index = 0
 
-    def move(self, dt):
+			# change tool
+			if keys[pygame.K_q] and not self.timers['tool switch'].active:
+				self.timers['tool switch'].activate()
+				self.tool_index += 1
+				self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
+				self.selected_tool = self.tools[self.tool_index]
 
-        # Normalizing a Vector
-        if self.direction.magnitude() > 0:
-            self.direction = self.direction.normalize()
+			# seed use
+			if keys[pygame.K_LCTRL]:
+				self.timers['seed use'].activate()
+				self.direction = pygame.math.Vector2()
+				self.frame_index = 0
 
-        # Horizontal Movement
-        self.pos.x += self.direction.x * self.speed * dt
-        self.rect.centerx = self.pos.x
-        
-        # Vertical Movement
-        self.pos.y += self.direction.y * self.speed * dt
-        self.rect.centery = self.pos.y
+			# change seed 
+			if keys[pygame.K_e] and not self.timers['seed switch'].active:
+				self.timers['seed switch'].activate()
+				self.seed_index += 1
+				self.seed_index = self.seed_index if self.seed_index < len(self.seeds) else 0
+				self.selected_seed = self.seeds[self.seed_index]
 
-    def update(self, dt):
-        self.input()
-        self.get_status()
-        self.updater_timers()
-        self.move(dt)
-        self.animate(dt)
+	def get_status(self):
+		
+		# idle
+		if self.direction.magnitude() == 0:
+			self.status = self.status.split('_')[0] + '_idle'
+
+		# tool use
+		if self.timers['tool use'].active:
+			self.status = self.status.split('_')[0] + '_' + self.selected_tool
+
+	def update_timers(self):
+		for timer in self.timers.values():
+			timer.update()
+
+	def collision(self, direction):
+		for sprite in self.collision_sprites.sprites():
+			if hasattr(sprite, 'hitbox'):
+				if sprite.hitbox.colliderect(self.hitbox):
+					if direction == 'horizontal':
+						if self.direction.x > 0: # moving right
+							self.hitbox.right = sprite.hitbox.left
+						if self.direction.x < 0: # moving left
+							self.hitbox.left = sprite.hitbox.right
+						self.rect.centerx = self.hitbox.centerx
+						self.pos.x = self.hitbox.centerx
+
+					if direction == 'vertical':
+						if self.direction.y > 0: # moving down
+							self.hitbox.bottom = sprite.hitbox.top
+						if self.direction.y < 0: # moving up
+							self.hitbox.top = sprite.hitbox.bottom
+						self.rect.centery = self.hitbox.centery
+						self.pos.y = self.hitbox.centery
+
+	def move(self,dt):
+
+		# normalizing a vector 
+		if self.direction.magnitude() > 0:
+			self.direction = self.direction.normalize()
+
+		# horizontal movement
+		self.pos.x += self.direction.x * self.speed * dt
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
+
+		# vertical movement
+		self.pos.y += self.direction.y * self.speed * dt
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
+
+	def update(self, dt):
+		self.input()
+		self.get_status()
+		self.update_timers()
+		self.get_target_pos()
+
+		self.move(dt)
+		self.animate(dt)
--- code/settings.py
+++ code/settings.py
@@ -4,9 +4,9 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
-# overlay positions
+# overlay positions 
 OVERLAY_POSITIONS = {
-	'tool' : (40, SCREEN_HEIGHT - 15),
+	'tool' : (40, SCREEN_HEIGHT - 15), 
 	'seed': (70, SCREEN_HEIGHT - 5)}
 
 PLAYER_TOOL_OFFSET = {
